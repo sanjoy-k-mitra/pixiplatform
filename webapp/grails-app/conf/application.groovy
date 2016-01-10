d7273e3bb3c5d5fe9cf431be937c8db0657176bd
sanjoy <sanjoy120@gmaiul.com>
2016-01-10 06:15:25 +0600

    heroku ready

--- webapp/grails-app/conf/application.groovy
+++ webapp/grails-app/conf/application.groovy
@@ -1,4 +1,16 @@
-
+environments {
+	production {
+		dataSource {
+			dbCreate = "update"
+			driverClassName = "org.postgresql.Driver"
+			dialect = org.hibernate.dialect.PostgreSQLDialect
+			uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+			url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+			username = uri.userInfo.split(":")[0]
+			password = uri.userInfo.split(":")[1]
+		}
+	}
+}
 
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.pixisolutions.pixiplatform.security.User'
